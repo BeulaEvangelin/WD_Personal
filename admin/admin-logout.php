801a0d1e746a93cd4e06d17a38e306183de9efe5
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-12-05 00:07:03 -0500

    admin logout create and style

---
+++ admin/admin-logout.php
@@ -0,0 +1,12 @@
+<?php session_start();  
+ session_destroy();
+?>
+
+
+<?php include('constants/header.php');?>
+
+<section class="section">
+<a href = "admin-login.php">LOGIN AGAIN<a>
+</section>
+
+<?php include('constants/footer.php');?>
